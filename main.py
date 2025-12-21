9867b4153a99d150e5b430e0fe1a2a246077cacc
Sjoerd473 <sjoerdbrussee@hotmail.com>
2025-12-21 18:07:17 +0100

    refactored for class syntax

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import time
 import tkinter as tk
 from tkinter import messagebox, ttk
 from tkinter.constants import HORIZONTAL
 
 import psycopg
 from modules.db import LoggerDB
-from modules.file_writer import print_to_file, refresh_file
 from modules.row_writer import Add_row
+from modules.timer_window import TimerWindow
 
 dsn = "dbname=logger user=postgres password=megablaat"
 
@@ -21,279 +20,293 @@ db = LoggerDB(dsn)
 # hourly rate on logs not subproject
 # window to mark things complete
 # third column for subsubproject
+#
+# another class for a mark for complete window
+#
 
 
-# modify for dicts?
-def refresh_listbox(cat, project=""):
-    if cat == "p":
-        projects = [item["name"] for item in db.get_projects()]
-        p_var.set(projects)
-    elif cat == "s":
-        subs = [item["name"] for item in db.get_subs(project)]
-        s_var.set(subs)
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.db = LoggerDB(dsn)
+        self.new_row = Add_row()
+        # these _ functions are automatically called when creating the class, thus creating the class creates the window
+        self._init_state()
+        self._build_ui()
+        self._bind_events()
 
+        self._initial_refresh()
 
-def get_project_name_from_list():
-    p_selection = p_list.curselection()
+    def _init_state(self):
+        self.p_var = tk.StringVar()
+        self.s_var = tk.StringVar()
+        self.error_var = tk.StringVar()
+        self.resultsContent = tk.StringVar()
 
-    if p_selection:
-        p_name = p_list.get(p_selection[0])
-        return p_name
+    def _build_ui(self):
+        self._build_project_column()
+        self._build_subproject_column()
+        self._build_payment_column()
+        self._build_error_label()
 
+    def _bind_events(self):
+        self._bind_project_events()
+        self._bind_subproject_events()
 
-def get_subproject_name_from_list():
-    selection = s_list.curselection()
+    def _initial_refresh(self):
+        self.refresh_listbox("p")
 
-    if selection:
-        s_name = s_list.get([selection[0]])
-        return s_name
+    # -----------------------------
+    # PROJECT COLUMN UI
+    # -----------------------------
 
+    def _build_project_column(self):
+        # Frame
+        self.p_col = ttk.Frame(self.root, padding=(12, 12, 12, 12))
+        self.p_col.grid(column=0, row=0, sticky="nwes")
 
-def remove_error():
-    error_var.set("")
+        # Label
+        self.p_lbl = ttk.Label(self.p_col, text="Project:")
+        self.p_lbl.grid(column=0, row=0)
 
+        # Listbox
+        self.p_list = tk.Listbox(
+            self.p_col, listvariable=self.p_var, exportselection=False
+        )
+        self.p_list.grid(column=0, row=1, rowspan=5)
 
-def show_error(msg):
-    error_var.set(msg)
-    root.after(5000, remove_error)
+        # Add-project label
+        self.pe_lbl = ttk.Label(self.p_col, text="Add a new project")
+        self.pe_lbl.grid(column=0, row=6)
 
+        # Entry
+        self.pe_ety = ttk.Entry(self.p_col)
+        self.pe_ety.grid(column=0, row=7)
 
-# def validate_name(p_name):
-#     if p_name in projects:
-#         show_error(f"{p_name} already in database!")
+        # Button
+        self.pe_btn = ttk.Button(self.p_col, text="Add", command=self.add_project)
+        self.pe_btn.grid(column=0, row=8)
 
+    # -----------------------------
+    # PROJECT COLUMN EVENTS
+    # -----------------------------
 
-# def schedule_check(event):
-#     root.after_cancel(schedule_check.job) if hasattr(schedule_check, "job") else None
-#     schedule_check.job = root.after(300, lambda: validate_name(pe_ety.get().strip()))
+    def _bind_project_events(self):
+        self.p_list.bind("<<ListboxSelect>>", self.update_subprojects)
 
+    # -----------------------------
+    # PROJECT COLUMN LOGIC
+    # -----------------------------
 
-# pe_ety.bind("<KeyRelease>", schedule_check)
+    def get_selected_project(self):
+        selection = self.p_list.curselection()
+        if selection:
+            return self.p_list.get(selection[0])
 
+    def add_project(self):
+        p_name = self.pe_ety.get().strip()
 
-def add_project():
-    p_name = pe_ety.get().strip()
+        p_names = [item["name"] for item in self.db.get_projects()]
 
-    p_names = [item["name"] for item in db.get_projects()]
-
-    if p_name in p_names:
-        show_error(f"{p_name} already in database!")
-        # this needs to fire on a typing event in the box
-    else:
-        try:
-            db.post_project(p_name)
-            pe_ety.delete(0, tk.END)
-            refresh_listbox("p")
-        except psycopg.IntegrityError as e:
-            db.conn.rollback()
-            messagebox.showerror(
-                "Duplicate Project", f"Project '{p_name}' already exists."
-            )
-        except Exception as e:
-            db.conn.rollback()
-            messagebox.showerror("Database Error", str(e))
-
-
-def add_subproject():
-    p_name = get_project_name_from_list()
-    p_subs = [item["name"] for item in db.get_subs(p_name)]
-    if p_name:
-        s_name = se_ety.get().strip()
-        if any(tup[0] == s_name for tup in p_subs):
-            show_error(f"{s_name} already in database!")
+        if p_name in p_names:
+            self.show_error(f"{p_name} already in database!")
+            # this needs to fire on a typing event in the box
         else:
             try:
-                se_ety.delete(0, tk.END)
-                db.post_sub(s_name, p_name)
-
-                refresh_listbox("s", p_name)
+                self.db.post_project(p_name)
+                self.pe_ety.delete(0, tk.END)
+                self.refresh_listbox("p")
             except psycopg.IntegrityError as e:
-                db.conn.rollback()
+                self.db.conn.rollback()
+                # does messagebox need self?
                 messagebox.showerror(
-                    "Duplicate Subproject",
-                    f"Project '{p_name}' already has a subproject named '{s_name}'.",
+                    "Duplicate Project", f"Project '{p_name}' already exists."
                 )
             except Exception as e:
-                db.conn.rollback()
+                self.db.conn.rollback()
                 messagebox.showerror("Database Error", str(e))
 
-    else:
-        show_error("Select a project first")
+    def update_subprojects(self, event):
+        p_name = self.get_project_name_from_list()
 
+        if p_name:
+            new_subs = [item["name"] for item in self.db.get_subs(p_name)]
 
-def update_subprojects(event):
-    p_name = get_project_name_from_list()
+            self.s_var.set(new_subs)
 
-    if p_name:
-        new_subs = [item["name"] for item in db.get_subs(p_name)]
-
-        s_var.set(new_subs)
-
-
-# change for payment from logs
-def update_payment(event):
-    s_name = get_subproject_name_from_list()
-
-    if s_name:
-        hourly_rate = db.get_hourly(s_name)["retribuizione"]
-
-        resultsContent.set(f"{hourly_rate}€")
-
-
-# change for payment from logs
-def alter_payment():
-    s_name = get_subproject_name_from_list()
-
-    if s_name:
-        n = b_ety.get().strip()
-        try:
-            hourly = int(n)
-
-            if hourly <= 0:
-                raise ValueError
-            b_ety.delete(0, tk.END)
-            db.update_hourly(n, s_name[0])
-            refresh_file(db.get_file_data())
-            update_payment("")
-        except ValueError:
-            messagebox.showerror(
-                "Invalid input",
-                "The hourly rate you entered must be a whole, positive, number.",
-            )
-        except Exception as e:
-            messagebox.showerror("Database Error", str(e))
-
-
-def open_timer(project, subproject):
-    timer_window = tk.Toplevel(root)
-    running = True
-    start_time = time.time()
-
-    def update_timer_txt():
-        if running:
-            elapsed = int(time.time() - start_time)
-            hours, remainder = divmod(elapsed, 3600)
-            minutes, seconds = divmod(remainder, 60)
-            timer_txt.config(text=f"{hours:02}:{minutes:02}:{seconds:02}")
-            timer_window.after(1000, update_timer_txt)
-
-    def stop_timer():
-        try:
-            timer_window.destroy()
-            new_row.end_logger()
-            db.post_log(new_row.post_data())
-            print_to_file(db.get_file_data())
-        except IndexError:
-            messagebox.showerror(
-                "Database Error",
-                "Something went wrong trying to write the data to a file.",
-            )
-
-    timer_content = ttk.Frame(timer_window, padding=(12, 12, 12, 12))
-
-    # timer = StringVar()
-    timer_lbl = ttk.Label(timer_window, text="You have been working for")
-    timer_txt = ttk.Label(timer_window, text="00:00:00")
-    project_txt = ttk.Label(
-        timer_window, text=f"The project you are working on is {project}"
-    )
-    subproject_txt = ttk.Label(
-        timer_window, text=f"The subproject you working on is {subproject}"
-    )
-    stop_timer_btn = ttk.Button(timer_window, text="Stop Timer", command=stop_timer)
-
-    timer_content.grid(column=0, row=0, padx=15, pady=15)
-    timer_lbl.grid(column=0, row=0)
-    timer_txt.grid(column=1, row=0, columnspan=2, sticky="we")
-    project_txt.grid(column=0, row=1, pady=10, padx=(5, 5))
-    subproject_txt.grid(column=1, row=1, pady=10, padx=(5, 5))
-    stop_timer_btn.grid(column=0, row=2, columnspan=2, sticky="we", pady=(10, 0))
-
-    update_timer_txt()
-
-
-def start_all():
-    try:
-        project = get_project_name_from_list()
-        subproject = get_subproject_name_from_list()
-        project_id = db.get_project_id(project)["id"]
-
-        subproject_id = db.get_subproject_id(subproject, project_id)["id"]
-        new_row.start_logger(project_id, subproject_id)
-        open_timer(project, subproject)
-    except TypeError:
-        messagebox.showerror(
-            "Missing Subproject",
-            "Unable to start a timer without chosing both a project and a subproject",
+    # -----------------------------
+    # SUBPROJECT COLUMN UI
+    # -----------------------------
+    def _build_subproject_column(self):
+        # Frame
+        self.s_col = ttk.Frame(self.root, padding=(12, 12, 12, 12))
+        self.s_col.grid(column=1, row=0)
+        # Label
+        self.s_lbl = ttk.Label(self.s_col, text="Subproject:")
+        self.s_lbl.grid(column=0, row=0)
+        # Listbox
+        self.s_list = tk.Listbox(
+            self.s_col, listvariable=self.s_var, exportselection=False
         )
+        self.s_list.grid(column=0, row=1, rowspan=5)
+
+        # Add-sub label
+        self.se_lbl = ttk.Label(self.s_col, text="Add a new subproject")
+        self.se_lbl.grid(column=0, row=7)
+        # Entry
+        self.se_ety = ttk.Entry(self.s_col)
+        self.se_ety.grid(column=0, row=8)
+        # Button
+        self.se_btn = ttk.Button(self.s_col, text="Add", command=self.add_subproject)
+        self.se_btn.grid(column=0, row=9)
+
+    # -----------------------------
+    # SUBPROJECT COLUMN EVENTS
+    # -----------------------------
+
+    def _bind_subproject_events(self):
+        self.s_list.bind("<<ListboxSelect>>", self.update_payment)
+
+    # -----------------------------
+    # SUBPROJECT COLUMN LOGIC
+    # -----------------------------
+
+    def add_subproject(self):
+        p_name = self.get_project_name_from_list()
+        p_subs = [item["name"] for item in self.db.get_subs(p_name)]
+        if p_name:
+            s_name = self.se_ety.get().strip()
+            if any(tup[0] == s_name for tup in p_subs):
+                self.show_error(f"{s_name} already in database!")
+            else:
+                try:
+                    self.se_ety.delete(0, tk.END)
+                    self.db.post_sub(s_name, p_name)
+
+                    self.refresh_listbox("s", p_name)
+                except psycopg.IntegrityError as e:
+                    self.db.conn.rollback()
+                    messagebox.showerror(
+                        "Duplicate Subproject",
+                        f"Project '{p_name}' already has a subproject named '{s_name}'.",
+                    )
+                except Exception as e:
+                    self.db.conn.rollback()
+                    messagebox.showerror("Database Error", str(e))
+
+        else:
+            self.show_error("Select a project first")
+
+    def update_payment(self, event): ...
+
+    # part of subproject
+
+    # -----------------------------
+    # BUTTON COLUMN UI
+    # -----------------------------
+    def _build_payment_column(self):
+        # Frame
+        self.b_col = ttk.Frame(self.root, padding=(12, 12, 12, 12))
+        self.b_col.grid(column=2, row=0)
+        # Label - text
+        self.b_lbl = ttk.Label(self.b_col, text="Hourly rate:")
+        self.b_lbl.grid(column=0, row=0)
+        # Label - rate
+        self.b_lbl_rate = ttk.Label(self.b_col, textvariable=self.resultsContent)
+        self.b_lbl_rate.grid(column=1, row=0, sticky="n", pady=(0, 25))
+        # Label - update rate
+        self.b_lbl_ety = ttk.Label(self.b_col, text="Update hourly rate")
+        self.b_lbl_ety.grid(column=0, row=1, columnspan=2, pady=5)
+        # Entry - update rate
+        self.b_ety = ttk.Entry(self.b_col)
+        self.b_ety.grid(column=0, row=2, columnspan=2, pady=5)
+        # Button - update rate
+        self.b_ety_btn = ttk.Button(
+            self.b_col, text="Update", command=self.alter_payment
+        )
+        self.b_ety_btn.grid(column=0, row=3, columnspan=2, pady=(0, 25))
+        # Button - start timer
+        self.b_start_btn = ttk.Button(
+            self.b_col, text="Start timer", command=self.start_all
+        )
+        self.b_start_btn.grid(column=0, row=7, columnspan=2, rowspan=2, pady=(80, 0))
+
+    # -----------------------------
+    # BUTTON COLUMN EVENTS
+    # -----------------------------
+
+    # -----------------------------
+    # BUTTON COLUMN LOGIC
+    # -----------------------------
+
+    def alter_payment(self):
+        ...
+        # part of column 3
+
+    def start_all(self):
+        try:
+            project = self.get_project_name_from_list()
+            subproject = self.get_subproject_name_from_list()
+            project_id = self.db.get_project_id(project)["id"]
+
+            subproject_id = self.db.get_subproject_id(subproject, project_id)["id"]
+            new_row.start_logger(project_id, subproject_id)
+
+            timer = TimerWindow(root, project, subproject, new_row, db)
+        except TypeError:
+            messagebox.showerror(
+                "Missing Subproject",
+                "Unable to start a timer without chosing both a project and a subproject",
+            )
+
+    # -----------------------------
+    # ERROR FRAME UI
+    # -----------------------------
+
+    def _build_error_label(self):
+        self.error_lbl = ttk.Label(
+            self.root, textvariable=self.error_var, foreground="red"
+        )
+        self.error_lbl.grid(column=0, row=1, columnspan=3)
+
+    # -----------------------------
+    # HELPER METHODS
+    # -----------------------------
+
+    def remove_error(self):
+        self.error_var.set("")
+
+    def show_error(self, msg):
+        self.error_var.set(msg)
+        root.after(5000, self.remove_error)
+
+    def refresh_listbox(self, cat, project=""):
+        if cat == "p":
+            projects = [item["name"] for item in self.db.get_projects()]
+            self.p_var.set(projects)
+        elif cat == "s":
+            subs = [item["name"] for item in self.db.get_subs(project)]
+            self.s_var.set(subs)
+
+    def get_project_name_from_list(self):
+        p_selection = self.p_list.curselection()
+
+        if p_selection:
+            p_name = self.p_list.get(p_selection[0])
+            return p_name
+
+    def get_subproject_name_from_list(self):
+        selection = self.s_list.curselection()
+
+        if selection:
+            s_name = self.s_list.get([selection[0]])
+            return s_name
 
 
-root = tk.Tk()
-root.title("Am I working too much?")
-
-p_col = ttk.Frame(root, padding=(12, 12, 12, 12))
-s_col = ttk.Frame(root, padding=(12, 12, 12, 12))
-b_col = ttk.Frame(root, padding=(12, 12, 12, 12))
+# this goes in a different file
 
 
-p_var = tk.StringVar(value=[item["name"] for item in db.get_projects()])
-s_var = tk.StringVar(value=[])
-
-p_lbl = ttk.Label(p_col, text="Project:")
-p_list = tk.Listbox(p_col, listvariable=p_var, exportselection=False)
-p_bar = ttk.Separator(p_col, orient=HORIZONTAL)
-pe_lbl = ttk.Label(p_col, text="Add a new project")
-pe_ety = ttk.Entry(p_col)
-pe_btn = ttk.Button(p_col, text="Add", command=add_project)
-
-p_list.bind("<<ListboxSelect>>", update_subprojects)
-
-s_lbl = ttk.Label(s_col, text="Subproject:")
-s_list = tk.Listbox(s_col, listvariable=s_var, exportselection=False)
-s_bar = ttk.Separator(s_col, orient=HORIZONTAL)
-se_lbl = ttk.Label(s_col, text="Add a new subproject")
-se_ety = ttk.Entry(s_col)
-se_btn = ttk.Button(s_col, text="Add", command=add_subproject)
-
-s_list.bind("<<ListboxSelect>>", update_payment)
-
-resultsContent = tk.StringVar()
-b_lbl = ttk.Label(b_col, text="Hourly rate:")
-b_lbl_rate = ttk.Label(b_col, textvariable=resultsContent)
-b_lbl_ety = ttk.Label(b_col, text="Update hourly rate")
-b_ety = ttk.Entry(b_col)
-b_ety_btn = ttk.Button(b_col, text="Update", command=alter_payment)
-b_start_btn = ttk.Button(b_col, text="Start timer", command=start_all)
-
-error_var = tk.StringVar()
-
-error_lbl = ttk.Label(root, textvariable=error_var, foreground="red")
-
-
-p_col.grid(column=0, row=0, sticky="nwes")
-p_lbl.grid(column=0, row=0)
-p_list.grid(column=0, row=1, rowspan=5)
-p_bar.grid(column=0, row=6, pady=5)
-pe_lbl.grid(column=0, row=7)
-pe_ety.grid(column=0, row=8)
-pe_btn.grid(column=0, row=9)
-
-s_col.grid(column=1, row=0)
-s_lbl.grid(column=0, row=0)
-s_list.grid(column=0, row=1, rowspan=5)
-s_bar.grid(column=0, row=6, pady=5)
-se_lbl.grid(column=0, row=7)
-se_ety.grid(column=0, row=8)
-se_btn.grid(column=0, row=9)
-
-b_col.grid(column=2, row=0)
-b_lbl.grid(column=0, row=0, sticky="n", pady=(0, 25))
-b_lbl_rate.grid(column=1, row=0, sticky="n", pady=(0, 25))
-b_lbl_ety.grid(column=0, row=1, columnspan=2, pady=5)
-b_ety.grid(column=0, row=2, columnspan=2, pady=5)
-b_ety_btn.grid(column=0, row=3, columnspan=2, pady=(0, 25))
-b_start_btn.grid(column=0, row=7, columnspan=2, rowspan=2, pady=(80, 0))
-
-error_lbl.grid(column=0, row=1, columnspan=3)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
---
+++ modules/timer_window.py
@@ -0,0 +1,83 @@
+import time
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+from modules.file_writer import print_to_file, refresh_file
+
+
+class TimerWindow:
+    def __init__(self, parent, project, subproject, new_row, db):
+        self.parent = parent
+        self.project = project
+        self.subproject = subproject
+        self.new_row = new_row
+        self.db = db
+
+        self.timer_window = tk.Toplevel(parent)
+        self.running = True
+        self.start_time = time.time()
+
+        self._build_ui()
+        self._update_timer()
+
+    def _update_timer(self):
+        if self.running:
+            elapsed = int(time.time() - self.start_time)
+            hours, remainder = divmod(elapsed, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            self.timer_txt.config(text=f"{hours:02}:{minutes:02}:{seconds:02}")
+            self.timer_window.after(1000, self._update_timer)
+
+    def _build_ui(self):
+        self._build_timer_frame()
+
+    def _build_timer_frame(self):
+        # Frame
+
+        self.timer_content = ttk.Frame(self.timer_window, padding=(12, 12, 12, 12))
+        self.timer_content.grid(column=0, row=0, padx=15, pady=15)
+
+        # Label - timer
+        self.timer_lbl = ttk.Label(self.timer_content, text="You have been working for")
+        self.timer_lbl.grid(column=0, row=0)
+
+        # Label - clock
+        self.timer_txt = ttk.Label(self.timer_content, text="00:00:00")
+        self.timer_txt.grid(column=1, row=0, columnspan=2, sticky="we")
+
+        # Label - project
+        self.project_txt = ttk.Label(
+            self.timer_content, text=f"The project you are working on is {self.project}"
+        )
+        self.project_txt.grid(column=0, row=1, pady=10, padx=(5, 5))
+
+        # Label - sub
+        self.subproject_txt = ttk.Label(
+            self.timer_content,
+            text=f"The subproject you working on is {self.subproject}",
+        )
+        self.subproject_txt.grid(column=1, row=1, pady=10, padx=(5, 5))
+
+        # Button - stop timer
+        self.stop_timer_btn = ttk.Button(
+            self.timer_content, text="Stop Timer", command=self.stop_timer
+        )
+        self.stop_timer_btn.grid(
+            column=0, row=2, columnspan=2, sticky="we", pady=(10, 0)
+        )
+
+    # -----------------------------
+    # BUTTON COLUMN LOGIC
+
+    # -----------------------------
+    def stop_timer(self):
+        try:
+            self.timer_window.destroy()
+            self.new_row.end_logger()
+            self.db.post_log(self.new_row.post_data())
+            print_to_file(self.db.get_file_data())
+        except IndexError:
+            messagebox.showerror(
+                "Database Error",
+                "Something went wrong trying to write the data to a file.",
+            )
